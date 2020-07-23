c7157f6879bc0fcc5949aded16f733d4233f3cc4
Victoria Erokhina <victoria@Victorias-MacBook-Pro.local>
2020-07-23 11:52:38 +0300

    Add binsearch by memory size

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,56 @@
+import java.io.File
 import java.nio.file.Paths
 import java.util.*
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
 import kotlin.concurrent.thread
+import java.util.regex.Pattern
 
 private val CLASS_PATH = System.getProperty("java.class.path")
 private val JAVA_PATH = Paths.get(System.getProperty("java.home")).resolve("bin").resolve("java").toString()
-private val JVM_OPTIONS = listOf("-Xmx5m", "-XX:+PrintGCDetails", "-Xloggc:/Users/victoria/gc.log")
+private val JVM_OPTIONS = listOf(/*"-Xmx4097k", */"-XX:+PrintGCDetails", "-Xloggc:/Users/victoria/gc.log")
+private val PATTERN = ".*GC\\(([^()]*)\\)"
 
-var value: AtomicInteger = AtomicInteger(0)
+private val MAX = 52428800 // 50mb
+
+var value: AtomicReference<Int> = AtomicReference(0)
 var random = Random()
+val pattern = Pattern.compile(PATTERN)
 
 fun main(args: Array<String>) {
-    val successful = runProcess(Process::class.java.name, JVM_OPTIONS, args)
+    // Warmup
+    repeat(1000) {
+        run(args, MAX)
+    }
+
+//    repeat(1) {
+//        run(listOf("printTime").toTypedArray(), MAX)
+//    }
+
+    var l = 0
+    var r = MAX
+
+    while (r - l > 1) {
+        val mid = (l + r) / 2
+
+        var error = false
+        try {
+            run(args, mid)
+        } catch (e : Exception) {
+            error = true
+        }
+
+        if (getGcNum() > 10 || error) {
+            l = mid
+        } else {
+            r = mid
+        }
+    }
+
+    println(l)
+}
+
+fun run(args: Array<String>, memorySize: Int) {
+    val successful = runProcess(Process::class.java.name, JVM_OPTIONS + "-Xmx$memorySize", args)
 
     if (!successful) {
         println("The benchmark failed with error, see the output.")
@@ -42,13 +81,26 @@ private class Process {
             val threads = (0 until 10).map { threadId ->
                 thread {
                     repeat(1000000) {
-                        val newVal = random.nextInt(1000)
+                        val newVal = random.nextInt(10000000)
                         value.set(newVal)
                     }
                 }
             }
             threads.forEach { it.join() }
-            print(System.currentTimeMillis() - t0)
+            if (args.isNotEmpty() && args[0] == "printTime") {
+                println(System.currentTimeMillis() - t0)
+            }
         }
     }
+}
+
+fun getGcNum():Int {
+    val text = File("/Users/victoria/gc.log").readText(Charsets.UTF_8)
+    val m = pattern.matcher(text)
+    var last = 0
+    while (m.find()) {
+        last = m.group(1).toInt()
+    }
+
+    return last
 }
